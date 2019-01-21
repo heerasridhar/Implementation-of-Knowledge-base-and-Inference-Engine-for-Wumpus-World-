f36b24575d9bcccdc50becdc42cb1e1c5381a4e5
Heera Sridhar <38568704+heerasridhar@users.noreply.github.com>
2019-01-21 17:25:59 -0600

    Add files via upload

---
+++ logical_expression.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------------
+# Name:        logical_expression
+# Purpose:     Contains logical_expression class, inference engine,
+#              and assorted functions
+#
+# Created:     09/25/2011
+# Last Edited: 07/22/2013  
+# Notes:       *This contains code ported by Christopher Conly from C++ code
+#               provided by Dr. Vassilis Athitsos
+#              *Several integer and string variables are put into lists. This is
+#               to make them mutable so each recursive call to a function can
+#               alter the same variable instead of a copy. Python won't let us
+#               pass the address of the variables, so put it in a list which is
+#               passed by reference. We can also now pass just one variable in
+#               the class and the function will modify the class instead of a
+#               copy of that variable. So, be sure to pass the entire list to a
+#               function (i.e. if we have an instance of logical_expression
+#               called le, we'd call foo(le.symbol,...). If foo needs to modify
+#               le.symbol, it will need to index it (i.e. le.symbol[0]) so that
+#               the change will persist.
+#              *Written to be Python 2.4 compliant for omega.uta.edu
+#-------------------------------------------------------------------------------
+
+import sys
+from copy import copy
+
+#-------------------------------------------------------------------------------
+# Begin code that is ported from code provided by Dr. Athitsos
+class logical_expression:
+    """A logical statement/sentence/expression class"""
+    # All types need to be mutable, so we don't have to pass in the whole class.
+    # We can just pass, for example, the symbol variable to a function, and the
+    # function's changes will actually alter the class variable. Thus, lists.
+    def __init__(self):
+        self.symbol = ['']
+        self.connective = ['']
+        self.subexpressions = []
+
+
+
+def print_expression(expression, separator):
+    """Prints the given expression using the given separator"""
+    if expression == 0 or expression == None or expression == '':
+        print '\nINVALID\n'
+
+    elif expression.symbol[0]: # If it is a base case (symbol)
+        sys.stdout.write('%s' % expression.symbol[0])
+
+    else: # Otherwise it is a subexpression
+        sys.stdout.write('(%s' % expression.connective[0])
+        for subexpression in expression.subexpressions:
+            sys.stdout.write(' ')
+            print_expression(subexpression, '')
+            sys.stdout.write('%s' % separator)
+        sys.stdout.write(')')
+
+
+def read_expression(input_string, counter=[0]):
+    """Reads the next logical expression in input_string"""
+    # Note: counter is a list because it needs to be a mutable object so the
+    # recursive calls can change it, since we can't pass the address in Python.
+    
+    result = logical_expression()
+    length = len(input_string)
+    while True:
+        if counter[0] >= length:
+            break
+
+        if input_string[counter[0]] == ' ':    # Skip whitespace
+            counter[0] += 1
+            continue
+
+        elif input_string[counter[0]] == '(':  # It's the beginning of a connective
+            counter[0] += 1
+            read_word(input_string, counter, result.connective)
+            read_subexpressions(input_string, counter, result.subexpressions)
+            break
+
+        else:  # It is a word
+            read_word(input_string, counter, result.symbol)
+            break
+    return result
+
+
+def read_subexpressions(input_string, counter, subexpressions):
+    """Reads a subexpression from input_string"""
+    length = len(input_string)
+    while True:
+        if counter[0] >= length:
+            print '\nUnexpected end of input.\n'
+            return 0
+
+        if input_string[counter[0]] == ' ':     # Skip whitespace
+            counter[0] += 1
+            continue
+
+        if input_string[counter[0]] == ')':     # We are done
+            counter[0] += 1
+            return 1
+
+        else:
+            expression = read_expression(input_string, counter)
+            subexpressions.append(expression)
+
+
+def read_word(input_string, counter, target):
+    """Reads the next word of an input string and stores it in target"""
+    word = ''
+    while True:
+        if counter[0] >= len(input_string):
+            break
+
+        if input_string[counter[0]].isalnum() or input_string[counter[0]] == '_':
+            target[0] += input_string[counter[0]]
+            counter[0] += 1
+
+        elif input_string[counter[0]] == ')' or input_string[counter[0]] == ' ':
+            break
+
+        else:
+            print('Unexpected character %s.' % input_string[counter[0]])
+            sys.exit(1)
+
+
+def valid_expression(expression):
+    """Determines if the given expression is valid according to our rules"""
+    if expression.symbol[0]:
+        return valid_symbol(expression.symbol[0])
+
+    if expression.connective[0].lower() == 'if' or expression.connective[0].lower() == 'iff':
+        if len(expression.subexpressions) != 2:
+            print('Error: connective "%s" with %d arguments.' %
+                        (expression.connective[0], len(expression.subexpressions)))
+            return 0
+
+    elif expression.connective[0].lower() == 'not':
+        if len(expression.subexpressions) != 1:
+            print('Error: connective "%s" with %d arguments.' %
+                        (expression.connective[0], len(expression.subexpressions)))
+            return 0
+
+    elif expression.connective[0].lower() != 'and' and \
+         expression.connective[0].lower() != 'or' and \
+         expression.connective[0].lower() != 'xor':
+        print('Error: unknown connective %s.' % expression.connective[0])
+        return 0
+
+    for subexpression in expression.subexpressions:
+        if not valid_expression(subexpression):
+            return 0
+    return 1
+
+
+def valid_symbol(symbol):
+    """Returns whether the given symbol is valid according to our rules."""
+    if not symbol:
+        return 0
+
+    for s in symbol:
+        if not s.isalnum() and s != '_':
+            return 0
+    return 1
+
+# End of ported code
+#-------------------------------------------------------------------------------
+
+# Add all your functions here
+
+
+def read_symbols(expression, symbols):
+    if expression.symbol[0]:
+        symbols.append(expression.symbol[0])
+    for subexpression in expression.subexpressions:
+        read_symbols(subexpression, symbols)
+
+def tt_check_all(kb, alpha, symbols, model):
+    if not symbols:
+        if pl_true(kb, model):
+            return pl_true(alpha, model)
+        else:
+            return True
+    first = symbols[0]
+    rest = symbols[1:]
+    return tt_check_all( kb, alpha, rest, extend(model, first, True) ) \
+           and tt_check_all( kb, alpha, rest, extend(model, first, False) )
+
+
+def pl_true(expression, model):
+    if expression.connective[0].lower() == 'and':
+        result = True
+        for i, subexpression in enumerate(expression.subexpressions):
+            if(i == 0):
+                result = pl_true(subexpression, model)
+                continue;
+            result = result and pl_true(subexpression, model)
+        return result
+    elif expression.connective[0].lower() == 'not':
+        result = not pl_true(expression.subexpressions[0], model)
+        return result
+    elif expression.connective[0].lower() == 'or':
+        result = True
+        for i, subexpression in enumerate(expression.subexpressions):
+            if(i == 0):
+                result = pl_true(subexpression, model)
+                continue;
+            result = result or pl_true(subexpression, model)
+        return result
+    elif expression.connective[0].lower() == 'xor':
+        result = True
+        for i, subexpression in enumerate(expression.subexpressions):
+            if(i == 0):
+                result = pl_true(subexpression, model)
+                continue;
+            result = result ^ pl_true(subexpression, model)
+        return result
+    elif expression.connective[0].lower() == 'if':
+        left = expression.subexpressions[0]
+        right = expression.subexpressions[1]
+        statement1 = pl_true(left, model)
+        statement2 = pl_true(right, model)
+        return ((not statement1) or statement2)
+    elif expression.connective[0].lower() == 'iff':
+        left = expression.subexpressions[0]
+        right = expression.subexpressions[1]
+        statement1 = pl_true(left, model)
+        statement2 = pl_true(right, model)
+        return ((not statement1) or statement2) and ((not statement2) or statement1)
+    return model[expression.symbol[0]]
+
+
+def tt_entails(knowledge_base, statement, negation, symbollist):
+    
+    try:
+        output = open('result.txt', 'w')
+    except:
+        print('unable to create output file')
+    list1 = []
+    list2 = []
+    model = symbollist.copy();
+    read_symbols(knowledge_base, list1)
+    read_symbols(statement, list2)
+    list1 = list(set(list1))
+    list2= list(set(list2))
+    list1.extend(list2)
+    symbols = list(set(list1))
+    for s in model.keys():
+        try:
+            symbols.remove(s)
+        except Exception:
+            pass
+    statement_true = tt_check_all(knowledge_base, statement, symbols, model)
+    statement_false = tt_check_all(knowledge_base, negation, symbols, model)
+    if statement_true == True and statement_false == False:
+        output.write('definitely true')
+    elif statement_true == False and statement_false == True:
+        output.write('definitely false')
+    elif statement_true == False and statement_false == False:
+        output.write('possibly true, possibly false')
+    elif statement_true == True and statement_false == True:
+        output.write('both true and false')
+
+    else:
+        output.write('Error')
+    print 
+    output.close()
+
+
+def extend(model, s, value):
+    model[s] = value
+    return model
---
+++ readme.txt
@@ -0,0 +1,20 @@
+NAME: HEERA SRIDHAR
+UTA ID: 1001529609
+PROGRAMMING LANGUAGE: Python
+
+CODE STRUCTURE:
+The following functions were used:
+
+1. tt_entails checks if the statement and its negation entail from the knowledge base. 
+2.pl_true() 
+3.The output is printed to a file result.txt.
+4.tt_check_all
+
+HOW TO RUN AND EXECUTE:
+
+First Execute check_true_false.py,
+
+The program should be invoked from the commandline as follows:
+
+ex: 
+      python check_true_false.py wumpus_rules.txt additional_knowledge.txt statement_file.txt
